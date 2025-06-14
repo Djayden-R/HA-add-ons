4c7849ecda8e3ffd527831729ba3d930cc2e6006
Djayden Rosel <6953@rgomiddelharnis.nl>
2025-06-14 10:07:51 +0200

    Update run.sh

--- Discord Bot test/run.sh
+++ Discord Bot test/run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-# Haal de token uit options.json
+# Haal Discord-token uit de Home Assistant add-on config (/data/options.json)
 DISCORD_TOKEN=$(jq -r '.discord_token' /data/options.json)
 
-# Zet 'm als env var en start de bot
+# Geef de token door als environment variable
 export DISCORD_TOKEN
+
+# Start de bot
 python3 /bot.py
