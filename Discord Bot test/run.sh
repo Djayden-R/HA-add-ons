d1b203d9182cfb2e11d43be7cc2cfcb688a32761
Djayden Rosel <6953@rgomiddelharnis.nl>
2025-06-14 10:06:48 +0200

    Update run.sh

--- Discord Bot test/run.sh
+++ Discord Bot test/run.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+
+# Haal de token uit options.json
+DISCORD_TOKEN=$(jq -r '.discord_token' /data/options.json)
+
+# Zet 'm als env var en start de bot
+export DISCORD_TOKEN
 python3 /bot.py
